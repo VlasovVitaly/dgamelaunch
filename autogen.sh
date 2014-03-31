05ad37d6245f7a4da28906e811efa0592a57c457
Vlasov Vitaly <vnigtha@gmail.com>
2014-04-01 01:51:27 +0400

    fix autogen script

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "Generating configuration files..."
-echo
 
 autoconf
 autoheader
 # evil
 rm -rf autom4te.cache
-./configure "$@"
+if [ $# -ge 1 ]; then
+    ./configure "$@"
+fi
